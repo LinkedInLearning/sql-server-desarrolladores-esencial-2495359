028e02227239bea8efbb97401e0d3515a509a708
Fangarita <112103737+Fangarita@users.noreply.github.com>
2022-12-23 10:27:26 -0500

    Delete Opciones para la creación de vistas ENCRYPTION.sql

--- Opciones para la creación de vistas ENCRYPTION.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW Purchasing.PurchaseOrderReject  
-WITH ENCRYPTION  
-AS  
-SELECT PurchaseOrderID, ReceivedQty, RejectedQty,   
-    RejectedQty / ReceivedQty AS RejectRatio, DueDate  
-FROM Purchasing.PurchaseOrderDetail  
-WHERE RejectedQty / ReceivedQty > 0  
-AND DueDate > CONVERT(DATETIME,'20010630',101) ;  
-GO  
